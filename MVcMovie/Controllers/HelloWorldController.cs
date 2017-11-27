3b591727f69c8b0b833083ac05d7ca0245742b24
Habib Khan <hkhan@smbhd.com>
2017-11-27 14:55:32 -0600

    init

---
+++ MVcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(String name, int numtimes=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is: {numtimes}");
+        }
+
+    }
+}
